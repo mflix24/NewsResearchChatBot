403f278dbbc954fc612706de70642c2531670b29
mflix24 <banglacourse1223@gmail.com>
2024-03-11 14:39:15 +0600

    codes are changed

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import warnings
+warnings.filterwarnings('ignore')
 import os
 import streamlit as st
 import pickle
 import time
 from langchain import OpenAI
+from langchain_openai import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
 from dotenv import load_dotenv
